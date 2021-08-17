6246793c612274d7f4d89fcd505a5dbe1c1305e3
Justinas Stankevicius <justinas@justinas.org>
2021-08-17 18:34:19 +0300

    Reduce DHCP range

--- main.tf
+++ main.tf
@@ -19,6 +19,32 @@ resource "libvirt_network" "k8s" {
   dns {
     enabled = true
   }
+
+  xml {
+    # By default, DHCP range is the whole subnet.
+    # We will eventually want virtual IPs, so try to make space for them.
+    # XSLT (I have no idea what I'm doing),
+    # because of https://github.com/dmacvicar/terraform-provider-libvirt/issues/794
+    xslt = <<EOF
+<?xml version="1.0" ?>
+<xsl:stylesheet version="1.0"
+                xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
+  <xsl:output omit-xml-declaration="yes" indent="yes"/>
+  <xsl:template match="node()|@*">
+     <xsl:copy>
+       <xsl:apply-templates select="node()|@*"/>
+     </xsl:copy>
+  </xsl:template>
+
+  <xsl:template match="/network/ip/dhcp/range">
+    <xsl:copy>
+      <xsl:attribute name="start">10.240.0.100</xsl:attribute>
+      <xsl:apply-templates select="@*[not(local-name()='start')]|node()"/>
+    </xsl:copy>
+  </xsl:template>
+</xsl:stylesheet>
+EOF
+  }
 }
 
 resource "libvirt_volume" "nixos_boot" {
