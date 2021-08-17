c9ecad83e5d0b2c06bb6d64bc2c39eb2df2e9927
Justinas Stankevicius <justinas@justinas.org>
2021-08-17 16:37:38 +0300

    init

---
+++ nixpkgs.nix
@@ -0,0 +1,6 @@
+builtins.fetchGit {
+  name = "nixos-21.05-2021-08-13";
+  url = "https://github.com/NixOS/nixpkgs";
+  ref = "refs/heads/nixos-21.05";
+  rev = "a445f5829889959d65ad65e5c961d5c67e1cd677";
+}
