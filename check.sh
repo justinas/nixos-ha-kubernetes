a8970e09b86615af7bd8556500a116c841fe440b
Justinas Stankevicius <justinas@justinas.org>
2021-08-18 20:02:58 +0300

    Add a basic test script

---
+++ check.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -ex
+
+etcd1_ip=$(jq -r '.values.root_module.child_modules[] | .resources[] | select(.values.name == "etcd1").values.network_interface[0].addresses[0]' show.json)
+
+etcdctl --endpoints "https://$etcd1_ip:2379" \
+    --ca-file ./certs/generated/etcd/ca.pem \
+    --cert-file ./certs/generated/etcd/peer.pem \
+    --key-file ./certs/generated/etcd/peer-key.pem \
+    member list | grep -q isLeader=true
+
+echo "Success."
