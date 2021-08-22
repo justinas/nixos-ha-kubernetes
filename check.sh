05aa84cd30714983896158d4f4a8e6fc53d4e787
Justinas Stankevicius <justinas@justinas.org>
2021-08-22 20:20:14 +0300

    Try running a pod in check.sh

--- check.sh
+++ check.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-set -ex
+set -e
 
 etcd1_ip=$(jq -r '.values.root_module.child_modules[] | .resources[] | select(.values.name == "etcd1").values.network_interface[0].addresses[0]' show.json)
 
@@ -7,8 +7,15 @@ etcdctl --endpoints "https://$etcd1_ip:2379" \
     --ca-file ./certs/generated/etcd/ca.pem \
     --cert-file ./certs/generated/etcd/peer.pem \
     --key-file ./certs/generated/etcd/peer-key.pem \
-    member list | grep -q isLeader=true
+    member list \
+    | tee /dev/stderr | grep -q isLeader=true
 
 k --request-timeout 1 cluster-info
+
+k run --rm --attach --restart Never \
+    --image busybox \
+    busybox \
+    --command id \
+    | tee /dev/stderr | grep -q "uid=0(root) gid=0(root) groups=10(wheel)"
 
 echo "Success."
